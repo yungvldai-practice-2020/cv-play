a5bf8cd32ecdaec91129335aa81dd8632b9d588e
Vlad Ivanov <vivanov@metaprime.ru>
2020-02-18 02:40:11 +0300

    detect eyes

--- detect.py
+++ detect.py
@@ -1,14 +1,17 @@
 import cv2 as cv
 
-face_cascade = cv.CascadeClassifier('./haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 image = cv.imread('face.jpg')
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x, y, w, h) in faces:
+for (x,y,w,h) in faces:
     cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = image[y:y+h, x:x+w]
-
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 cv.imshow('Detect', image)
 cv.waitKey(0)
 cv.destroyAllWindows()
