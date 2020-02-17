26725e38f5912150d108c860e1ec27e39d9d62a7
Vlad Ivanov <vivanov@metaprime.ru>
2020-02-18 00:04:23 +0300

    threshold

---
+++ treshold.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+
+image = cv.imread("./image.jpg")
+gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+ret, threshold_image = cv.threshold(gray_image, 127, 255, cv.THRESH_BINARY)
+cv.imshow("Dog & cat wb", threshold_image)
+cv.waitKey(0)
+cv.destroyAllWindows()
