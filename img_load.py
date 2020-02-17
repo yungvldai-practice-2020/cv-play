4e4aec1530b305da54d1a03ec6f90cba096d6012
Vlad Ivanov <vivanov@metaprime.ru>
2020-02-17 23:04:16 +0300

    cv alias

--- img_load.py
+++ img_load.py
@@ -1,5 +1,5 @@
-import cv2
-image = cv2.imread("./image.jpg")
-cv2.imshow("Dog & cat", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import cv2 as cv
+image = cv.imread("./image.jpg")
+cv.imshow("Dog & cat", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
